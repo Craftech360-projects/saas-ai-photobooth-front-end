8a7a491cfeda5711f343fdd2bf57680f91142bea
SAC <sachin@craftech360.com>
2024-05-22 12:28:59 +0530

    updated-code

--- src/Swap.jsx
+++ src/Swap.jsx
@@ -1,109 +1,154 @@
-/* eslint-disable no-unused-vars */
+// import React, { useState, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { initializeApp } from "firebase/app";
+// import { getDatabase, ref, onChildAdded,off  } from "firebase/database";
+
+// function Swap() {
+//   const [sourceImageUrl, setSourceImageUrl] = useState("");
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     // Firebase initialization
+    // const firebaseConfig = {
+    //   apiKey: "AIzaSyA_GpMgnjA1Em0eway6c0W90i6w4K8o4YE",
+    //   authDomain: "sac-project-4b5ab.firebaseapp.com",
+    //   databaseURL: "https://sac-project-4b5ab-default-rtdb.firebaseio.com",
+    //   projectId: "sac-project-4b5ab",
+    //   storageBucket: "sac-project-4b5ab.appspot.com",
+    //   messagingSenderId: "413553081982",
+    //   appId: "1:413553081982:web:7edf544b047a4f88ab222f"
+    // };
+//     const firebaseApp = initializeApp(firebaseConfig);
+//     const database = getDatabase(firebaseApp);
+
+//     // Listen for new child added to 'images' node in Firebase Realtime Database
+//     const databaseRef = ref(database, "images");
+//     const handleChildAdded = (snapshot) => {
+//       const newImage = snapshot.val();
+//       if (newImage && newImage.url) {
+//         setSourceImageUrl(newImage.url); // Update the state with the new image URL
+//       } else {
+//         console.error("New source image URL not found in database.");
+//         navigate("/error");
+//       }
+//     };
+
+//     // Attach the event listener
+//     onChildAdded(databaseRef, handleChildAdded);
+
+//     // Clean up the listener when the component unmounts
+//     return () => {
+//       // Detach the event listener
+//       return () => {
+//         off(imagesRef, 'child_added', handleChildAdded); // Correct way to remove the listener
+//       };
+//     };
+//   }, [navigate]);
+
+
+  
+//   return (
+//     <div>
+//       {sourceImageUrl ? (
+//         <div>
+//           <h1>Image Loaded</h1>
+//           <img src={sourceImageUrl} alt="Newly Added" style={{ width: "100%" }} />
+//         </div>
+//       ) : (
+//         <h1>Waiting for image to be added in Firebase realtime DB...</h1>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Swap;
 import React, { useState, useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";// Ensure this path matches the location of your data.json file
+import { useNavigate } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onChildAdded, off } from "firebase/database";
 import { supabase } from "./supabaseClient";
-import ImageSelectionForm from "./components/ImageSelectionForm";
 
 function Swap() {
-  const [enhance, setEnhance] = useState(false);
-  const [targetImage, setTargetImage] = useState(null);
+  const [sourceImageUrl, setSourceImageUrl] = useState("");
   const navigate = useNavigate();
-  const location = useLocation();
-  const [sourceImageUrl, setSourceImageUrl] = useState('');
-  const sourceImageBlob = location.state?.sourceImage;
+  const targetImage = 'times_square_thor.jpeg'; // Manually set the target image
+  const enhance = true; // Example state for enhancing image
 
   useEffect(() => {
-    if (!sourceImageBlob) {
-      console.error("Source image is not provided.");
-      navigate("/"); // Navigate back to capture if no source image is found
-    }
+    const firebaseConfig = {
+      apiKey: "AIzaSyA_GpMgnjA1Em0eway6c0W90i6w4K8o4YE",
+      authDomain: "sac-project-4b5ab.firebaseapp.com",
+      databaseURL: "https://sac-project-4b5ab-default-rtdb.firebaseio.com",
+      projectId: "sac-project-4b5ab",
+      storageBucket: "sac-project-4b5ab.appspot.com",
+      messagingSenderId: "413553081982",
+      appId: "1:413553081982:web:7edf544b047a4f88ab222f"
+    };
+    const firebaseApp = initializeApp(firebaseConfig);
+    const database = getDatabase(firebaseApp);
+    const databaseRef = ref(database, "images");
 
-    // Create an object URL for the Blob and update state
-    const objectURL = URL.createObjectURL(sourceImageBlob);
-    setSourceImageUrl(objectURL);
-
-    // Clean up the object URL when the component unmounts
-    return () => {
-      URL.revokeObjectURL(objectURL);
+    const handleChildAdded = async (snapshot) => {
+      const newImage = snapshot.val();
+      if (newImage && newImage.url) {
+        setSourceImageUrl(newImage.url);
+        console.log(newImage.url);
+        performSwap(newImage.url); // Call performSwap function when new image URL is added
+      } else {
+        console.error("New source image URL not found in database.");
+      }
     };
 
-  }, [sourceImageBlob, navigate]);
+    onChildAdded(databaseRef, handleChildAdded);
 
-  const selectImage = (imagesSrc) => {
-    console.log('imagesSrcccccccccccc', imagesSrc);
-    setTargetImage(imagesSrc);
-  };
+    return () => {
+      off(databaseRef, 'child_added', handleChildAdded);
+    };
+  }, [navigate]);
 
+  const performSwap = async (sourceImageUrl) => {
+    navigate("/loading");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    navigate("/loading"); // Assume a loading route
-
-    // Assuming targetImage is the result from selecting a random image and not the original blob
     try {
+      const response = await fetch(sourceImageUrl);
+      const sourceImageBlob = await response.blob();
+
       const formData = new FormData();
-      formData.append(
-        "targetImage",
-        new File([sourceImageBlob], "sourceImage.jpg", { type: "image/jpeg" })
-      );
+      formData.append("targetImage", new File([sourceImageBlob], "sourceImage.jpg", { type: "image/jpeg" }));
       formData.append("enhance", JSON.stringify(enhance));
 
-      // Replace 'targetImage' with the actual image URL or path you intend to swap with
-      if (targetImage) {
-        const response = await fetch(targetImage);
-        const targetImageBlob = await response.blob();
-        formData.append(
-          "sourceImage",
-          new File([targetImageBlob], "targetImage.jpg", { type: "image/jpeg" })
-        );
-      }
+      const targetResponse = await fetch(targetImage);
+      const targetImageBlob = await targetResponse.blob();
+      formData.append("sourceImage", new File([targetImageBlob], "targetImage.jpg", { type: "image/jpeg" }));
 
-      const swapResponse = await fetch(
-        "http://localhost:8000/api/swap-face/",
-        {
-          method: "POST",
-          body: formData,
-        }
-      );
+      const swapResponse = await fetch("http://192.168.1.47:8000/api/swap-face/", {
+        method: "POST",
+        body: formData,
+      });
 
       if (!swapResponse.ok) {
         throw new Error("Something went wrong with the swap API call");
       }
 
       const swappedImageBlob = await swapResponse.blob();
-
-      // Convert the swapped image to JPEG
       const convertedBlob = await convertImageToJPEG(swappedImageBlob);
 
-      // Upload the swapped and converted image to Supabase
       const fileName = `swapped-images/${Date.now()}-result.jpg`;
-      const { error: uploadError } = await supabase.storage
-        .from("images")
-        .upload(fileName, convertedBlob, {
-          contentType: "image/jpeg",
-        });
+      const { error: uploadError } = await supabase.storage.from("images").upload(fileName, convertedBlob, { contentType: "image/jpeg" });
 
       if (uploadError) {
         throw uploadError;
       }
 
       const publicURL = `https://mxyippuwkpysdexmxrbm.supabase.co/storage/v1/object/public/images/${fileName}`;
-
-      if (publicURL) {
-        navigate("/result", { state: { resultImageUrl: publicURL } });
-      } else {
-        console.error("Failed to get public URL");
-        navigate("/error"); 
-      }
+      navigate("/result", { state: { resultImageUrl: publicURL } });
     } catch (error) {
       console.error("Error:", error);
-      navigate("/error"); 
+      navigate("/error");
     }
   };
 
-  // Function to convert an image blob to JPEG format
-  function convertImageToJPEG(blob) {
+  const convertImageToJPEG = (blob) => {
     return new Promise((resolve, reject) => {
       const canvas = document.createElement("canvas");
       const ctx = canvas.getContext("2d");
@@ -113,22 +158,24 @@ function Swap() {
         canvas.width = img.width;
         canvas.height = img.height;
         ctx.drawImage(img, 0, 0);
-        canvas.toBlob(resolve, "image/jpeg"); // Convert the canvas to a blob in JPEG format
+        canvas.toBlob(resolve, "image/jpeg");
       };
 
       img.onerror = reject;
-      img.src = URL.createObjectURL(blob); // Create a URL for the blob and set it as the image source
+      img.src = URL.createObjectURL(blob);
     });
-  }
+  };
 
   return (
     <div>
-      <ImageSelectionForm
-        handleSubmit={handleSubmit}
-        enhance={enhance}
-        setEnhance={setEnhance}
-        selectImage={selectImage}
-      />
+      {sourceImageUrl ? (
+        <div>
+          <h1>Image Loaded</h1>
+          <img src={sourceImageUrl} alt="Newly Added" style={{ width: "100%" }} />
+        </div>
+      ) : (
+        <h1>Waiting for image to be added in Firebase realtime DB...</h1>
+      )}
     </div>
   );
 }
