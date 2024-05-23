10ece5c936400ea7287aa6c2347a36b5eb888d34
SAC <sachin@craftech360.com>
2024-05-23 13:29:46 +0530

    updated-conf

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // This will set the host to 0.0.0.0
+    port: 8080, // You can change this port to whatever you prefer
+  },
 })
